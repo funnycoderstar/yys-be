9152ae78d11d4964fcd9615be9867752da289a78
王亚星 <funnycoderstar@gmail.com>
2017-03-03 11:27:20 +0800

    feat: 将获取url模块化

--- crawler/fetchHeroUrl.js
+++ crawler/fetchHeroUrl.js
@@ -1,23 +1,25 @@
 const Crawler = require('crawler');
-const c = new Crawler({
-    maxConnections: 10,
-    callback: function (error, res, done) {
-        if (error) {
-            return;
-        } else {
-            const $ = res.$;
-
-            const $tables = $('table a');
-            // console.log($tables[0].attribs.href);
-            const tablesContent = [];
-            for (let i = 0; i < $tables.length; i++) {
-                const tableText = $tables[i].attribs.href;
-                tablesContent.push(tableText.split('\n').filter(text => text));
+module.export = function () {
+    return new Promise((resolve, reject) => {
+        const c = new Crawler({
+            maxConnections: 10,
+            callback: function (error, res, done) {
+                if (error) {
+                    reject(error);
+                    return;
+                } else {
+                    const $ = res.$;
+                    const $tables = $('table a');
+                    const tablesContent = [];
+                    for (let i = 0; i < $tables.length; i++) {
+                        const tableText = $tables[i].attribs.href;
+                        tablesContent.push(tableText.split('\n').filter(text => text));
+                    }
+                    resolve(tablesContent);
+                }
+                done();
             }
-            console.log(tablesContent);
-
-        }
-        done();
-    }
-});
-c.queue('http://www.18183.com/yys/201609/699803.html');
+        });
+        c.queue('http://www.18183.com/yys/201609/699803.html');
+    });
+};
