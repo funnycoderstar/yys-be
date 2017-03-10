4aeed90c452fa5cb607c6aad0f0ce0a355cad38e
王亚星 <funnycoderstar@gmail.com>
2017-03-10 23:04:28 +0800

    feat: 添加koa-static模块

--- src/app.js
+++ src/app.js
@@ -2,10 +2,13 @@ const koa = require('koa');
 const logger = require('koa-logger');
 const json = require('koa-json');
 const hero = require('./routers/hero');
+const serve  = require('koa-static');
+const path = require('path');
 
 const app = new koa();
 app.use(logger());
 app.use(json());
+app.use(serve(path.resolve(__dirname, '../public/')));
 
 app.use(hero.routes());
 app.use(hero.allowedMethods());
