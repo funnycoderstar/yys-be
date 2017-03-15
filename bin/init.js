0c7194744cd8639537cb2e6981aad24dd8beadda
王亚星 <funnycoderstar@gmail.com>
2017-03-15 22:43:29 +0800

    feat: 异步捕获mongoose异常

---
+++ bin/init.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+mongoose.Promise = Promise;
+mongoose.connect('mongodb://127.0.0.1:27017/test', (err) => {
+    if (err) {
+        console.log('数据库连接失败,请连接数据库服务!');
+        process.exit(0);
+    }
+});
+
+const updateHerosData = require('../src/tasks/updateHerosData');
+updateHerosData()
+.then(() => {
+    console.log('update end');
+    process.exit(0);
+});
--- src/test.js
+++
@@ -1,6 +0,0 @@
-const mongoose = require('mongoose');
-mongoose.connect('mongodb://127.0.0.1:27017/test');
-mongoose.Promise = Promise;
-
-const updateHerosData = require('./tasks/updateHerosData');
-updateHerosData();
