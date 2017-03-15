5491251ea86d5caf219b49cd79d72f6b6e91c5f4
王亚星 <funnycoderstar@gmail.com>
2017-03-15 23:42:54 +0800

    feat: 定时执行任务

--- src/main.js
+++ src/main.js
@@ -1,7 +1,17 @@
 const app = require('./app');
 const mongoose = require('mongoose');
+const schedule = require('node-schedule');
+const updateHerosData = require('../src/tasks/updateHerosData');
 mongoose.connect('mongodb://127.0.0.1:27017/test');
 mongoose.Promise = Promise;
+
+const rule = new schedule.RecurrenceRule();
+rule.hour = [0, 6, 12, 18];
+schedule.scheduleJob(rule, function () {
+    updateHerosData();
+    const now = new Date();
+    console.log(now.getFullYear() + '年' + now.getHours + '月' + now.getDate() + '日' + now.getHours + '时' + now.getMinutes + '分' + now.getSeconds + '秒' ,'获取完成');
+});
 
 app.listen(6600, () => {
     console.log('server is run');
