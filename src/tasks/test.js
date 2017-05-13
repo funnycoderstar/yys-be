40864010862dcc11bb31cf18409fea1be855be49
王亚星 <funnycoderstar@gmail.com>
2017-05-13 22:41:24 +0800

    feat: 删除测试的目录

---
+++ src/tasks/test.js
@@ -0,0 +1,8 @@
+// const fetchHeroMatch = require('../../crawler/fetchHeroGongluo');
+// console.log(fetchHeroMatch());
+// const fetchHeroMatch = require('../../crawler/fetchHeroVideo');
+// console.log(fetchHeroMatch());
+// const fetchHeroMatch = require('../../crawler/fetchLagou');
+// console.log(fetchHeroMatch());
+// const fetchHeroMatch = require('./updateHerosData');
+// console.log(fetchHeroMatch());
