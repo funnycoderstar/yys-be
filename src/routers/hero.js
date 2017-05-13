fd4fe6dc5c6685e590f68b5268a21717c8a270d5
王亚星 <funnycoderstar@gmail.com>
2017-05-13 22:21:18 +0800

    feat: 添加请求式神视频和式神攻略的后端Api

--- src/routers/hero.js
+++ src/routers/hero.js
@@ -2,6 +2,8 @@ const Router = require('koa-router');
 require('koa-cors');
 const router = new Router();
 const Hero = require('../models/hero');
+const HeroStrategy = require('../models/heroStrategy');
+const HeroVideo = require('../models/heroVideo');
 router
     .get('/heros', async (ctx) => {
         ctx.body = await Hero.find().sort({ 'rarity': -1 });
@@ -16,6 +18,14 @@ router
                 msg: '未查找到该式神'
             };
         }
+    })
+    .get('/heroStrategy', async (ctx) => {
+        ctx.body = await HeroStrategy.find();
+        ctx.status = 200;
+    })
+    .get('/heroVideo', async (ctx) => {
+        ctx.body = await HeroVideo.find();
+        ctx.status = 200;
     });
 
 module.exports = router;
