f1a68a923457a0f7c615746c7caaeb1de3714c69
王亚星 <funnycoderstar@gmail.com>
2017-05-14 16:11:11 +0800

    添加es6-promisify

--- src/tasks/updateHerosData.js
+++ src/tasks/updateHerosData.js
@@ -3,13 +3,25 @@ const fetchHeroUrl = require('../../crawler/fetchHeroUrl');
 const Hero = require('../models/hero');
 const imageDownloader = require('../utils/imageDownloader');
 const path = require('path');
-// const fs = require('fs');
+const fs = require('fs');
+const promisify = require('es6-promisify');
 
 module.exports = async function () {
     // 式神数据
     const herosUrl = await fetchHeroUrl();
     for (const url of herosUrl) {
         const heroData = await fetchHeroData(url);
+        let stat = null;
+        const publicPath = path.resolve(__dirname, '../../public');
+        try {
+            stat = await promisify(fs.stat)(publicPath);
+        } catch (err) {
+            await promisify(fs.mkdir)(publicPath);
+        }
+        if (stat && !stat.isDirectory()) {
+            await promisify(fs.unlink)(publicPath);
+            await promisify(fs.mkdir)(publicPath);
+        }
         await imageDownloader(
             /^http/.test(heroData.heroImg) ? heroData.heroImg : 'http:' + heroData.heroImg,
             path.resolve(__dirname, `../../public/${heroData.name}.jpg`)
